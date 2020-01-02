97011c0a9be4d31a1ef26fafae2db5ca28de06b4
1633 <1049803789@qq.com>
2020-01-02 16:56:31 +0800

    first commit code

---
+++ spring-boot-api/src/main/java/com/liusxg/springbootapi/IDubboProvider.java
@@ -0,0 +1,10 @@
+package com.liusxg.springbootapi;
+
+/**
+ * Created by liusxg on 2018/5/10.
+ */
+public interface IDubboProvider {
+
+    Response sayHello();
+    Response saySomething(String content);
+}
